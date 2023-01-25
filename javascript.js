add20c9a5ad742e7562b590489fd23411b9f8a01
Connor <connorzalud@gmail.com>
2023-01-24 20:40:52 -0500

    added classes for player color, classes for winning and losing marks

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const square = document.querySelector("#one");
 const square2 = document.querySelector("#two");
 const squares = document.querySelectorAll(".square");
+const header = document.querySelector(".header-container");
 
 let board = [0,1,2,3,4,5,6,7,8]
 
@@ -13,26 +14,8 @@ const player2 = playerFactory("player 2", "O", "false");
 
 
 squares.forEach(squares =>{
-    squares.addEventListener("click",function(e){
-        console.log(e.target.dataset.array);
-        if(player1.turn==="true"){
-            e.target.textContent = player1.mark;
-            board.splice(e.target.dataset.array,1, player1.mark);
-        } else{
-            e.target.textContent = player2.mark
-            board.splice(e.target.dataset.array,1,player2.mark)
-        }
-
-        checkArray();
-    
-        if(player1.turn === "true"){
-            player2.turn ="true";
-            player1.turn ="false"
-        } else{
-            player2.turn ="false";
-            player1.turn="true"
-        }
-    })})
+    squares.addEventListener("click", clickSquare
+        )})
 
 
 function checkArray(){
@@ -51,13 +34,66 @@ for (i=0; i<wins.length;i++){
     let [a,b,c] = wins[i];
     if(board[a] && board[a]===board[b] && board[a]===board[c]){
         win = board[a];
-        break
+        squares[a].classList.replace("x-mark","win")||squares[a].classList.replace("o-mark","win");
+        squares[b].classList.replace("x-mark","win")||squares[b].classList.replace("o-mark","win");
+        squares[c].classList.replace("x-mark","win")||squares[c].classList.replace("o-mark","win");
+        gameOver(win);
+
+        for (j=0;j<squares.length;j++){
+            if(!squares[j].classList.contains("win")){
+                squares[j].classList.replace("x-mark", "lose")||squares[j].classList.replace("o-mark","lose");
+            }
+            }
+        
     }
 }
 
-return console.log(win)
+
+
+ 
+
+return win
 
 }
 
-function gameOver(){
+function clickSquare(e){
+    console.log(e.target.dataset.array);
+        if(player1.turn==="true"){
+            e.target.textContent = player1.mark;
+            e.target.classList.add("x-mark");
+            board.splice(e.target.dataset.array,1, player1.mark);
+        } else{
+            e.target.textContent = player2.mark
+            e.target.classList.add("o-mark")
+            board.splice(e.target.dataset.array,1,player2.mark)
+        }
+
+        checkArray();
+    
+        if(player1.turn === "true"){
+            player2.turn ="true";
+            player1.turn ="false"
+        } else{
+            player2.turn ="false";
+            player1.turn="true"
+        }
+    }
+
+
+function gameOver(win){
+
+    const winText = document.createElement("div");
+    winText.setAttribute("class","win-text"); 
+    squares.forEach(squares=>
+        squares.removeEventListener("click", clickSquare))
+    if(win === "X"){
+       winText.innerText = `${player1.name} won!`;
+       header.append(winText);
+
+    } else {
+       winText.innerText = `${player2.name} won!`;
+       header.append(winText);
+    }
 }
+
+
